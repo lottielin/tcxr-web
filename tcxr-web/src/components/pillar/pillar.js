6d498c4d1c74b59a2b237bf464bfc7b47007cc4b
Charlotte Lin <charlottelin@berkeley.edu>
2022-04-02 21:57:33 -0700

    setup pillar file

--- tcxr-web/src/components/pillar/pillar.js
+++ tcxr-web/src/components/pillar/pillar.js
@@ -1,8 +1,46 @@
-class Pillar extends Component {
-  state = {};
-  render() {
-    return <div>Pillar</div>;
-  }
-}
+import { Button } from "bootstrap";
+
+const Pillar = (props) => {
+  const pillars = [
+    {
+      pillarName: "Creating Bias-Free Classrooms",
+      pillarValue: [
+        { identity: "teacher", value: "teacher1" },
+        { identity: "student", value: "student1" },
+        { identity: "admin", value: "admin1" },
+        { identity: "parent", value: "parent1" },
+      ],
+    },
+    {
+      pillarName: "Virtual Reality Education Platform",
+      pillarValue: [
+        { identity: "teacher", value: "teacher2" },
+        { identity: "student", value: "student2" },
+        { identity: "admin", value: "admin2" },
+        { identity: "parent", value: "parent2" },
+      ],
+    },
+    {
+      pillarName: "Real-Time Class Analytics",
+      pillarValue: [
+        { identity: "teacher", value: "teacher3" },
+        { identity: "student", value: "student3" },
+        { identity: "admin", value: "admin3" },
+        { identity: "parent", value: "parent3" },
+      ],
+    },
+    {
+      pillarName: "Student-Instructor Communication",
+      pillarValue: [
+        { identity: "teacher", value: "teacher4" },
+        { identity: "student", value: "student4" },
+        { identity: "admin", value: "admin4" },
+        { identity: "parent", value: "parent4" },
+      ],
+    },
+  ];
+
+  return <div>Pillars</div>;
+};
 
 export default Pillar;
--- tcxr-web/src/pages/landing/landing.js
+++ tcxr-web/src/pages/landing/landing.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import UserInput from "../../components/userInput/userInput";
+import Pillar from "../../components/pillar/pillar";
 import "./landing.css";
 
 class Landing extends Component {
@@ -22,7 +23,16 @@ class Landing extends Component {
           <p>
             Target viewer: <b>{this.state.target}</b>
           </p>
+          <p>TO DO 1: route to different pages based on target viewer</p>
+          {/* if routing to diff pages: 
+          1) in-page rendering with Link (make each button in userInput a link)
+          2) use optional params to label */}
+          <p>TO DO 2: dynamically render different versions of pillars</p>
           <UserInput setTarget={this.handleSetTarget} />
+        </div>
+
+        <div className="pillarContainer">
+          <Pillar identity={this.state.target} />
         </div>
       </React.Fragment>
     );
